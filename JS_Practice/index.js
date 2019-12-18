0fc727f566acd4c5bdd3721db836c2b51dce3554
venkatesh-sriramdas <svenkatesh.dotnet@gmail.com>
2019-12-18 15:25:47 +0530

    first commit

---
+++ JS_Practice/index.js
@@ -0,0 +1,41 @@
+function SignUp() {
+    var Name = document.getElementById("name").value;
+    var Pwd = document.getElementById("pwd").value;
+    console.clear();
+    if(Name =='' || Pwd =='')
+    {
+        alert('name and pwd are mandatory fields..');
+        console.log('Validation failed...');
+        return false;
+    }
+
+    console.log("started saving in local storate");
+
+    //Checking whether local storage is exists or not?
+    var isExist = false;
+    
+    var Candidates=[];
+    // var Candidate=[{name:'test',pwd:'test',comments:['test comments']}];
+    // Candidates.push(Candidate);
+
+    var existingData='';
+    
+    if (localStorage.getItem("CandStorage") != null) {
+       // isExist = true;
+          //getting Exising values
+          Candidates = JSON.parse(localStorage.getItem("CandStorage"));
+          
+    }
+            // Candidates = JSON.parse(localStorage.getItem("CandStorage"));
+        // Candidates.push({ name:  Name , pwd: Pwd ,comments:'' });
+        Candidates.push({ name:  Name , pwd: Pwd ,comments:'' });
+        localStorage.setItem("CandStorage",JSON.stringify(Candidates));
+    
+  
+
+
+    console.log("Local storage created on name of :" + "CandStorage");
+
+    alert('local storate created');
+
+}
